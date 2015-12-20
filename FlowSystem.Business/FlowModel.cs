b47fe1942e2f87d04946a8ee04f824f1aee901d5
Coen Stange <coen.stange@hotmail.com>
2015-12-20 16:42:27 +0100

    + Added interface of FlowModel

---
+++ FlowSystem.Business/FlowModel.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using FlowSystem.Business.Interfaces;
+using FlowSystem.Common;
+using FlowSystem.Common.Interfaces;
+
+namespace FlowSystem.Business
+{
+    public class FlowModel : IFlowModel
+    {
+        public FlowNetworkEntity FlowNetwork { get; set; }
+        public void AddMerger(PointEntity point)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddPipe(IFlowOutput start, IFlowInput end, IList<PointEntity> path)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddPump(PointEntity point)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddSink(PointEntity point)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddSplitter(PointEntity point)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ComponentPropertyChanged(IComponent component)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteComponent(IComponent component)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DuplicateComponent(IComponent component, PointEntity point)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool FileAlreadyExist(string path)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void MoveComponent(IComponent component, PointEntity point)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OpenFile(string path)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SaveFile(string path)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ FlowSystem.Business/Interfaces/IFlowModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using FlowSystem.Common;
+using FlowSystem.Common.Interfaces;
+
+namespace FlowSystem.Business.Interfaces
+{
+    public interface IFlowModel
+    {
+        FlowNetworkEntity FlowNetwork { get; set; }
+        void AddMerger(PointEntity point);
+        void AddPipe(IFlowOutput start, IFlowInput end, IList<PointEntity> path);
+        void AddPump(PointEntity point);
+        void AddSink(PointEntity point);
+        void AddSplitter(PointEntity point);
+        void ComponentPropertyChanged(IComponent component);
+        void DeleteComponent(IComponent component);
+        void DuplicateComponent(IComponent component, PointEntity point);
+        bool FileAlreadyExist(string path);
+        void MoveComponent(IComponent component, PointEntity point);
+        void OpenFile(string path);
+        void SaveFile(string path);
+    }
+}
