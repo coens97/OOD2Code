a3a827efd6a5902a77968630e02a785b2871c451
Coen Stange <coen.stange@hotmail.com>
2015-12-20 14:47:06 +0100

    +Create initial projects

--- FlowSystem.Presentation/App.xaml.cs
+++ FlowSystem.Presentation/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace FlowSystem
+namespace FlowSystem.Presentation
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- FlowSystem.Presentation/MainWindow.xaml.cs
+++ FlowSystem.Presentation/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace FlowSystem
+namespace FlowSystem.Presentation
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
